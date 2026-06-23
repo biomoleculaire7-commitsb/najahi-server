e5b57540e032d7687ac9e88c1131deb71ca4fef3
biomoleculaire7-commitsb <biomoleculaire7@gmail.com>
2026-06-23 21:12:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function geminiAsk(prompt) {
     });
     const options = {
       hostname: 'generativelanguage.googleapis.com',
-      path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${key}`,
+      path: `/v1beta/models/gemini-2.0-flash:generateContent?key=${key}`,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
